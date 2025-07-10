a2082e2e44b9334a294ce1a8b65557461e9e1de3
QKing <162024807+QKing-Official@users.noreply.github.com>
2025-07-10 14:34:29 +0200

    Update cart.blade.php

--- themes/default/views/components/cart.blade.php
+++ themes/default/views/components/cart.blade.php
@@ -1,7 +1,13 @@
 @if($cartCount > 0)
 <div class="w-10 h-10 flex items-center justify-center rounded-lg hover:bg-neutral transition">
-    <x-navigation.link :href="route('cart')">
-        <x-ri-shopping-bag-4-fill class="size-4" />
+    <x-navigation.link :href="route('cart')" aria-label="View cart">
+        <div class="relative">
+            <x-ri-shopping-bag-4-fill class="size-6" />
+
+                <span class="absolute top-0 right-0 translate-x-1/2 -translate-y-1/2 bg-red-600 text-white rounded-full text-[10px] font-semibold w-5 h-5 flex items-center justify-center select-none pointer-events-none leading-none">
+                    {{ $cartCount > 99 ? '99+' : $cartCount }}
+                </span>
+        </div>
     </x-navigation.link>
 </div>
 @endif
